f1bc5469cbee9c2418f0bd4c6c6e0d994f880a21
Samuel Vicente <samuel.eduardo.vicente@gmail.com>
2019-07-16 00:39:14 -0500

    Contact form

---
+++ src/components/Form.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles(theme => ({
+    '@global': {
+        body: {
+            backgroundColor: theme.palette.common.white,
+        },
+    },
+    paper: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+export default function Form() {
+    const classes = useStyles();
+
+    return (
+        <Container id='contactSection' maxWidth="xs">
+            <h1 style={{ fontFamily: 'Raleway' }}>Contact</h1>
+            <h1 className="hr" >{}</h1>
+            <div className={classes.paper}>
+                <form className={classes.form} noValidate>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField
+                                inputProps={{ maxLength: 70 }}
+                                autoComplete="name"
+                                name="name"
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="name"
+                                label="Name"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                inputProps={{ maxLength: 600 }}
+                                multiline
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="comments"
+                                label="Comments"
+                                id="comments"
+                                autoComplete="comments"
+                                rows="4"
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Submit
+                    </Button>
+                </form>
+            </div>
+        </Container>
+    );
+}
