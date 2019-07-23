48864be03d45295bf6218515d7762b6919423a1c
Samuel Vicente <samuel.eduardo.vicente@gmail.com>
2019-07-23 12:45:08 -0500

    Made mobile friendly

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 import { Container, Grid, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+    column: {
+        height: '95%',
+        width: '50%',
+        [theme.breakpoints.down('sm')]: {
+          width: '100%'
+        },
+    },
+    columnOne: {
+        height: '95%',
+        width: '50%',
+        display: 'none',
+        [theme.breakpoints.up('md')]: {
+          display: 'flex',
+        },
+    }
+  }))
 
 export default function About() {
+    const classes = useStyles()
     return (
         <Container id='aboutSection' style={{
             display: 'flex',
@@ -11,22 +31,18 @@ export default function About() {
         }}>
             <h1 style={{ fontFamily: 'Raleway', width: '100%' }}>About</h1>
             <h1 className="hr" >{}</h1>
-            <Container style={{
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
-            }}>
-                <Grid item xs={12}>
+            <Grid container alignContent="center" alignItems="center" spacing={10}>
+                <Grid item className={classes.column}>
                     <Typography>
                     Mission-driven full stack developer with a passion for thoughtful UI design, collaboration, and learning. I’ve always sought out opportunities and challenges that are meaningful to me. Although my professional path has taken many twists and turns, from being a musician and audio engineer to being a Software Quality Assurance analyst at a Software company, to being a Software developer and entrepreneur. I've never stopped engaging my passion to help others and solve problems. As a Full Stack developer, I enjoy using my attention to detail, my undeniable love for making things, and my work ethic to literally change the world. That's why I’m excited to make a big impact at a high growth company.
                 </Typography>
                 </Grid>
-                <Grid item xs={12}>
+                <Grid item className={classes.columnOne}>
                     <div>
                         <img style={{ width: 300, height: 400 }} src="images/Sammy.JPG" alt="Sammy" />
                     </div>
                 </Grid>
-            </Container>
+            </Grid>
         </Container>
     )
 }
